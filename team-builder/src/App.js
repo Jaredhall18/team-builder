412a5b6164faefd24058948e9401e6e35bbc0862
jaredhall18 <jaredhall18@gmail.com>
2021-09-14 11:52:14 -0600

    created react document

---
+++ team-builder/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import React, { useState } from "react";
+
+const initialformValues = {
+  name:"",
+  email:"",
+  role:"",
+}
+
+function App() {
+  const [team, setTeam] = useState([])
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
