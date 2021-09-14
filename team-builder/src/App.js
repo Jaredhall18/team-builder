854c8d31db1ba80dbf78213a9208aa6a1325a256
jaredhall18 <jaredhall18@gmail.com>
2021-09-14 12:54:38 -0600

    created updateForm functionality to app.js. Fields now accept data. Working on submit next.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -29,7 +29,7 @@ function App() {
   const [formValues, setFormValues] =useState(initialFormValues)
 
   const updateForm = (inputName, inputValue) => {
-  
+    setFormValues({ ...formValues, [inputName]: inputValue })
   }
 
   const submitForm = () => {
